14d7dbeb9f7965b806f575972298dfd04c6d7f82
aishu-18 <aishu72345@gmail.com>
2025-04-27 07:23:45 +0530

    git commit -m 'Added all files to repository'

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import NavBar from './components/NavBar';
+import ShareProjectForm from './components/ShareProjectForm';
+import Forum from './components/Forum';
+import Login from './components/Auth/Login';
+import SignUp from './components/Auth/SignUp';
+import Home from './components/Home';
+import ProjectsPage from './pages/ProjectsPage'; // Add this line
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/share-project" element={<ShareProjectForm />} />
+        <Route path="/forum" element={<Forum />} />
+        <Route path="/projects" element={<ProjectsPage />} /> {/* Important */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
